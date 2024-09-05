dc099c204ada2546620bc4facdd0848559e63942
Gabriel Santos <131888357+Gabits13@users.noreply.github.com>
2024-09-05 17:07:25 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -104,8 +104,31 @@
     }
   });
 }
+// Função para verificar se os elementos estão visíveis na viewport
+function checkVisibility2() {
+  const sections = document.querySelectorAll('.fade-in-sectionx');
+  sections.forEach(section => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top < window.innerHeight && rect.bottom > 0) {
+      section.classList.add('visible');
+    }
+  });
+}
+function checkVisibility3() {
+  const sections = document.querySelectorAll('.fade-in-sectionx2');
+  sections.forEach(section => {
+    const rect = section.getBoundingClientRect();
+    if (rect.top < window.innerHeight && rect.bottom > 0) {
+      section.classList.add('visible');
+    }
+  });
+}
 
 // Verificar visibilidade ao carregar a página
 window.addEventListener('load', checkVisibility);
+window.addEventListener('load', checkVisibility2);
+window.addEventListener('load', checkVisibility3);
 // Verificar visibilidade ao rolar a página
 window.addEventListener('scroll', checkVisibility);
+window.addEventListener('scroll', checkVisibility2);
+window.addEventListener('scroll', checkVisibility3);
